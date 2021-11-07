800d37968476d34176869b8598965968c252d1e9
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-07 20:18:39 +0530

    Added: snake and ladder player at zeroth position program

---
+++ src/com/snakeladder/demo/Snake_Ladder_UC1.java
@@ -0,0 +1,14 @@
+package com.snakeladder.demo;
+
+public class Snake_Ladder_UC1 {
+
+public static void main(String[] args) {
+
+	System.out.println("Welcome to Snake and Ladder Game ");
+	int player=0;
+	System.out.println("player initial position :" +player);
+	
+	
+}	
+	
+}
